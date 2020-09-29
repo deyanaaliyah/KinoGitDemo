8a81019023537d197cc3fbd7d7cb74f61e90bbfd
deyanaaliyah <62359528+deyanaaliyah@users.noreply.github.com>
2020-09-29 15:21:26 +0200

    init

---
+++ src/main/java/com/example/demo/KinoGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoGitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoGitDemoApplication.class, args);
+    }
+
+}
